9427bf2f0c18995b188ad2c9b0f96a4c810b7f9f
amirhagai <amir.hagai@mail.huji.ac.il>
2024-02-13 15:07:03 +0200

    basic torch3d play

---
+++ tools/3d_play.py
@@ -0,0 +1,123 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import os
+import sys
+import torch
+import pytorch3d
+
+import os
+import torch
+import matplotlib.pyplot as plt
+
+
+import requests
+
+# URL of the Python script
+url = 'https://raw.githubusercontent.com/facebookresearch/pytorch3d/main/docs/tutorials/utils/plot_image_grid.py'
+# Local path to save the script
+filepath = '/app/tools/plot_image_grid.py'
+
+response = requests.get(url)
+if response.status_code == 200:
+    with open(filepath, 'w') as f:
+        f.write(response.text)
+else:
+    print("Failed to download the script")
+
+from plot_image_grid import image_grid
+
+# Util function for loading meshes
+from pytorch3d.io import load_objs_as_meshes, load_obj
+
+# Data structures and functions for rendering
+from pytorch3d.structures import Meshes
+from pytorch3d.vis.plotly_vis import AxisArgs, plot_batch_individually, plot_scene
+from pytorch3d.vis.texture_vis import texturesuv_image_matplotlib
+from pytorch3d.renderer import (
+    look_at_view_transform,
+    FoVPerspectiveCameras, 
+    PointLights, 
+    DirectionalLights, 
+    Materials, 
+    RasterizationSettings, 
+    MeshRenderer, 
+    MeshRasterizer,  
+    SoftPhongShader,
+    TexturesUV,
+    TexturesVertex
+)
+
+# add path for demo utils functions 
+import sys
+import os
+sys.path.append(os.path.abspath(''))
+
+
+dir_path = '/app/mmrotate/3Ddata/meshes'
+os.makedirs(dir_path, exist_ok=True)
+
+
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+    torch.cuda.set_device(device)
+else:
+    device = torch.device("cpu")
+
+# Set paths
+DATA_DIR = "/app/mmrotate/3Ddata/"
+obj_filename = os.path.join(DATA_DIR, "meshes/Container.obj")
+
+# Load obj file
+mesh = load_objs_as_meshes([obj_filename], device=device)
+
+
+# Initialize a camera.
+# With world coordinates +Y up, +X left and +Z in, the front of the cow is facing the -Z direction. 
+# So we move the camera by 180 in the azimuth direction so it is facing the front of the cow. 
+R, T = look_at_view_transform(30.7, 60, 140) 
+cameras = FoVPerspectiveCameras(device=device, R=R, T=T)
+
+# Define the settings for rasterization and shading. Here we set the output image to be of size
+# 512x512. As we are rendering images for visualization purposes only we will set faces_per_pixel=1
+# and blur_radius=0.0. We also set bin_size and max_faces_per_bin to None which ensure that 
+# the faster coarse-to-fine rasterization method is used. Refer to rasterize_meshes.py for 
+# explanations of these parameters. Refer to docs/notes/renderer.md for an explanation of 
+# the difference between naive and coarse-to-fine rasterization. 
+raster_settings = RasterizationSettings(
+    image_size=1024, 
+    blur_radius=0.0, 
+    faces_per_pixel=1, 
+    bin_size=0,  # Use naive rasterization
+    max_faces_per_bin=None  # Optionally, adjust this value based on your needs
+)
+
+# Place a point light in front of the object. As mentioned above, the front of the cow is facing the 
+# -z direction. 
+lights = PointLights(device=device, location=[[0.0, 0.0, -3.0]])
+
+# Create a Phong renderer by composing a rasterizer and a shader. The textured Phong shader will 
+# interpolate the texture uv coordinates for each vertex, sample from a texture image and 
+# apply the Phong lighting model
+renderer = MeshRenderer(
+    rasterizer=MeshRasterizer(
+        cameras=cameras, 
+        raster_settings=raster_settings
+    ),
+    shader=SoftPhongShader(
+        device=device, 
+        cameras=cameras,
+        lights=lights
+    )
+)
+
+images = renderer(mesh)
+plt.figure(figsize=(10, 10))
+plt.imshow(images[0, ..., :3].cpu().numpy())
+plt.axis("off")
+
+plt.savefig("/app/mmrotate/3Ddata/rendering_images/first_try.png")
+print("done")
+
+
+
+
+
