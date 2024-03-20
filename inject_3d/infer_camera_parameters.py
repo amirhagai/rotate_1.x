0c58c6993c79b977aecb93d3ebc4ced68fa7a774
amirhagai <amir.hagai@mail.huji.ac.il>
2024-03-20 14:53:01 +0200

    test pre-commit

--- inject_3d/infer_camera_parameters.py
+++ inject_3d/infer_camera_parameters.py
@@ -1,12 +1,8 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-import os
-from pathlib import Path
-
 import numpy as np
 import torch
 from PIL import Image
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+
 
 # Util function for loading meshes
 from pytorch3d.io import load_objs_as_meshes
@@ -59,17 +55,16 @@ def find_angle_from_bbox(
 
 
 class InjectedObject:
-
     def __init__(self, obj_file_path, TZ_start=70, T=None) -> None:
 
         if torch.cuda.is_available():
-            device = torch.device('cuda:0')
-            torch.cuda.set_device(device)
+            self.device = torch.device('cuda:0')
+            torch.cuda.set_device(self.device)
         else:
-            device = torch.device('cpu')
+            self.device = torch.device('cpu')
 
-        self.mesh = load_objs_as_meshes([obj_file_path], device=device)
-        self.verts = mesh.verts_packed()  # Get the vertices of the mesh
+        self.mesh = load_objs_as_meshes([obj_file_path], device=self.device)
+        self.verts = self.mesh.verts_packed()  # Get the vertices of the mesh
         self.TZ_start = TZ_start
 
         self.elev = 90
@@ -85,10 +80,12 @@ class InjectedObject:
         if T is not None:
             self.base_T = T
 
-        self.base_R, self.base_R = self.base_R.to(device), self.base_R.to(device)
+        self.base_R, self.base_R = self.base_R.to(self.device), self.base_R.to(
+            self.device
+        )
 
         self.camera = FoVPerspectiveCameras(
-            device=device, R=self.base_R, T=self.base_T
+            device=self.device, R=self.base_R, T=self.base_T, zfar=500
         )
 
         self.f2 = (self.camera.zfar * self.camera.znear) / (
@@ -147,15 +144,23 @@ class InjectedObject:
 
         # verts = self.mesh.verts_packed()
 
-        x_max_y_max_z_min = self.find_closest(x_max, y_max, z_min)  # bl
-        x_min_y_max_z_min = self.find_closest(x_min, y_max, z_min)  # br
-        x_max_y_max_z_max = self.find_closest(x_max, y_max, z_max)  # tl
-        x_min_y_max_z_max = self.find_closest(x_min, y_max, z_max)  # tr
+        x_max_y_max_z_min = torch.stack((x_max, y_max, z_min))  # bl
+        # self.find_closest(x_max, y_max, z_min)
+        x_min_y_max_z_min = torch.stack((x_min, y_max, z_min))  # br
+        # self.find_closest(x_min, y_max, z_min)
+        x_max_y_max_z_max = torch.stack((x_max, y_max, z_max))  # tl
+        # self.find_closest(x_max, y_max, z_max)
+        x_min_y_max_z_max = torch.stack((x_min, y_max, z_max))  # tr
+        # self.find_closest(x_min, y_max, z_max)
 
-        x_max_y_min_z_min = self.find_closest(x_max, y_min, z_min)
-        x_min_y_min_z_min = self.find_closest(x_min, y_min, z_min)
-        x_min_y_min_z_max = self.find_closest(x_min, y_min, z_max)
-        x_max_y_min_z_max = self.find_closest(x_max, y_min, z_max)
+        x_max_y_min_z_min = torch.stack((x_max, y_min, z_min))
+        # self.find_closest(x_max, y_min, z_min)
+        x_min_y_min_z_min = torch.stack((x_min, y_min, z_min))
+        # self.find_closest(x_min, y_min, z_min)
+        x_min_y_min_z_max = torch.stack((x_min, y_min, z_max))
+        # self.find_closest(x_min, y_min, z_max)
+        x_max_y_min_z_max = torch.stack((x_max, y_min, z_max))
+        # self.find_closest(x_max, y_min, z_max)
 
         return (
             x_max_y_max_z_min,  # bl
@@ -183,7 +188,7 @@ class InjectedObject:
         if R is None:
             R = R_
         cameras = FoVPerspectiveCameras(
-            device=device, R=R, T=T, aspect_ratio=aspect_ratio
+            device=self.device, R=R, T=T, aspect_ratio=aspect_ratio, zfar=500
         )
 
         raster_settings = RasterizationSettings(
@@ -194,13 +199,15 @@ class InjectedObject:
             max_faces_per_bin=None,
         )
 
-        lights = PointLights(device=device, location=[[0.0, 0.0, -3.0]])
+        lights = PointLights(device=self.device, location=[[0.0, 0.0, -3.0]])
 
         renderer = MeshRenderer(
             rasterizer=MeshRasterizer(
                 cameras=cameras, raster_settings=raster_settings
             ),
-            shader=SoftPhongShader(device=device, cameras=cameras, lights=lights),
+            shader=SoftPhongShader(
+                device=self.device, cameras=cameras, lights=lights
+            ),
         )
 
         images = renderer(self.mesh)
@@ -256,7 +263,7 @@ class InjectedObject:
         )
 
         R = self.get_R(angle)
-        R = R.to(device)
+        R = R.to(self.device)
 
         V_R_1, V_R_2, V_R_3 = self._get_rotated_verts(verts, R)
 
@@ -318,7 +325,7 @@ class InjectedObject:
             rotation = RotateAxisAngle(
                 angle=angle, axis=self.rotation_axis
             ).get_matrix()
-            R_final = torch.bmm(rotation[:, :3, :3].to(device), self.base_R)
+            R_final = torch.bmm(rotation[:, :3, :3].to(self.device), self.base_R)
         else:
             R_final = self.base_R
         R = R_final[:, :3, :3]
@@ -422,7 +429,7 @@ class InjectedObject:
         # xs = 1 - ((pixels_values[:, 1].to(torch.float64)   * 2.) / width)
         ys = 1 - ((pixels_values[:, 0].to(torch.float64) * 2.0) / height)
         # ys = ((pixels_values[:, 0].to(torch.float64) * 2.) / height ) - 1
-        return xs.to(device), ys.to(device)
+        return xs.to(self.device), ys.to(self.device)
 
     def find_T_Z(
         self, pixels_values, verts, R, image_shape, angle=0, aspect_ratio=1.0
@@ -517,8 +524,6 @@ class InjectedObject:
         dy, dx = new_center[0] - bbox_center[0], new_center[1] - bbox_center[1]
         return dy, dx
 
-
-
     def align_aspect_ratio(self, aspect_ratio):
 
         current_ratio = self.natural_aspect_ratio
@@ -572,7 +577,6 @@ class InjectedObject:
         )
         centerlized_pix = rotated_pix + torch.stack([dy, dx])
 
-
         bl = centerlized_pix[0]
         br = centerlized_pix[1]
         tl = centerlized_pix[2]
@@ -584,13 +588,12 @@ class InjectedObject:
 
         aspect_ratio = self.align_aspect_ratio(aspect_ratio=aspect_ratio)
 
-
         T_Z = self.find_T_Z(
             centerlized_pix,
             extreme_verts,
             torch.eye(3)[None, :, :],
             image_shape,
-            aspect_ratio = 1. / aspect_ratio,
+            aspect_ratio=1.0 / aspect_ratio,
         )
 
         T_X, T_Y = self.find_T_X_T_Y(
@@ -599,35 +602,46 @@ class InjectedObject:
             R=R,
             image_shape=image_shape,
             T_Z=T_Z,
-            aspect_ratio = 1 / aspect_ratio,
+            aspect_ratio=1 / aspect_ratio,
         )
 
         if return_aspect_ratio is True:
             return R, torch.tensor([[T_X, T_Y, T_Z]]), extreme_pixels, aspect_ratio
         return R, torch.tensor([[T_X, T_Y, T_Z]]), extreme_pixels
-    
 
-    def __call__(self, top_left, top_right, bottom_left, bottom_right, image_shape, path, i, debug_draw=True, debug_T=False):
+    def __call__(
+        self,
+        top_left,
+        top_right,
+        bottom_left,
+        bottom_right,
+        image_shape,
+        path,
+        i,
+        debug_draw=False,
+    ):
 
         bbox = torch.stack([bottom_left, bottom_right, top_left, top_right])
-
         angle = find_angle_from_bbox(
             top_left, bottom_left, top_right, bottom_right, degrees=True
         )
-        print(f"found angle - {angle}")
-        bbox = self.rotate_pixels(bbox.to(torch.float32), theta=torch.tensor([(np.pi * angle) / 180])) # now the bbox is axis aligned 
+        bbox = self.rotate_pixels(
+            bbox.to(torch.float32), theta=torch.tensor([(np.pi * angle) / 180])
+        )  # now the bbox is axis aligned
 
         R, T, extreme_pixels, aspect_ratio = self.find_R_T_for_injection(
-            top_left=bbox[2], top_right=bbox[3], bottom_left=bbox[0], bottom_right=bbox[1], image_shape=image_shape, return_aspect_ratio=True
+            top_left=bbox[2],
+            top_right=bbox[3],
+            bottom_left=bbox[0],
+            bottom_right=bbox[1],
+            image_shape=image_shape,
+            return_aspect_ratio=True,
         )
-        if debug_T:
-            print(f"call found T to be - {T}")
 
         image = self.render_mesh(
             angle=angle, T_z=3, T=T, R=self.base_R, aspect_ratio=aspect_ratio
         )
-        # image = (image * 255).cpu().numpy().astype(np.uint8)
-        print()
+
         image = image.permute(2, 0, 1)
         image = torchvision.transforms.functional.rotate(image, angle.item(), fill=1)
         image = image.permute(1, 2, 0)
@@ -637,8 +651,8 @@ class InjectedObject:
         # image = np.transpose(image, (2, 0, 1))
 
         bbox = torch.stack([bottom_left, bottom_right, top_left, top_right])
-
-        Image.fromarray(image).save(f'{path}/bbox_{i}_angle{angle:.1f}.png')
+        angle = angle.item()
+        # Image.fromarray(image).save(f'{path}/bbox_{i}_angle{angle:.1f}.png')
         if debug_draw:
             draw_pixels(
                 image,
@@ -647,273 +661,11 @@ class InjectedObject:
                 square_size=5,
                 paint_color=np.array([255, 0, 0]).astype(np.uint8),
             )
-            Image.fromarray(image).save(f'{path}/bbox_{i}_angle{angle:.1f}_debug.png')
-        Image.fromarray(segmantation_mask * 255).save(f'{path}/bbox_{i}_angle{angle:.1f}_seg.png')
-
-        alpha_chanel = segmantation_mask[:, :, 0][:, :, None] * 255
-        rgba = np.dstack([image, alpha_chanel])
-        Image.fromarray(rgba, 'RGBA').save(f'{path}/bbox_{i}_angle{angle:.1f}_rgba.png')
-        return image, segmantation_mask
-
-
-def test_injection_sanity_check(obj_filename, app_path):
-
-    global IMAGE_SIZE
-
-    path = f'{app_path}/data/test_injected/trainval/images/sanity_check'
-    os.makedirs(path, exist_ok=True)
-
-    for angle in [0, 20, 50, 45, 78][::-1]:
-        print(f'angle - {angle}')
-        for i, T in enumerate([
-            torch.tensor([[5.0, 5.0, 50.0]]),
-            torch.tensor([[5.0, 7.0, 40.0]]),
-            torch.tensor([[2.0, 3.0, 50.0]]),
-            torch.tensor([[5.0, -3.0, 20.0]]),
-            torch.tensor([[-5.0, -6.0, 40.0]]),
-            torch.tensor([[-2.0, 7.0, 50.0]]),
-            torch.tensor([[2.0, -3.0, 60.0]]),
-            torch.tensor([[-5.0, 1.0, 80.0]]),
-            torch.tensor([[0.0, 0.0, 30.0]]),
-        ]):
-
-            print(f'\treal T - {T}')
-            # tr, bl, br, tl
-            injection_object = InjectedObject(obj_filename, TZ_start=T[0, 2], T=T)
-
-            pixels_to_highlight, verts_ = injection_object.get_extreme_pixels(
-                angle=angle, image_shape=(IMAGE_SIZE, IMAGE_SIZE, 3)
-            )
-
-            injection_object = InjectedObject(obj_filename)
-
-            (
-                R_new,
-                T_new,
-                extreme_pixels,
-            ) = injection_object.find_R_T_for_injection(
-                bottom_left=pixels_to_highlight[0],
-                top_right=pixels_to_highlight[3],
-                top_left=pixels_to_highlight[2],
-                bottom_right=pixels_to_highlight[1],
-                image_shape=[3, IMAGE_SIZE, IMAGE_SIZE],
-            )
-
-            image = injection_object.render_mesh(
-                angle=angle, T_z=3, T=T_new, R=R_new
-            )
-            image = (image * 255).cpu().numpy().astype(np.uint8)
-
-            draw_pixels(
-                image,
-                pixels_to_highlight[:, 0].numpy().astype(np.int32),
-                pixels_to_highlight[:, 1].numpy().astype(np.int32),
-                square_size=5,
-                paint_color=np.array([255, 0, 0]).astype(np.uint8),
-            )
 
             Image.fromarray(image).save(
-                f'{path}/T_{i}_angle_{angle}.png'
+                f'{path}/bbox_{i}_angle_{angle:.2f}.png'
             )
+        # alpha_chanel = segmantation_mask[:, :, 0][:, :, None] * 255
+        # rgba = np.dstack([image, alpha_chanel])
 
-            injection_object(bottom_left=pixels_to_highlight[0],
-                top_right=pixels_to_highlight[3],
-                top_left=pixels_to_highlight[2],
-                bottom_right=pixels_to_highlight[1],
-                image_shape=[3, IMAGE_SIZE, IMAGE_SIZE], path=path, i=i, debug_T=True)
-
-            print(f'\tfound T - {T_new}')
-            print()
-
-        print()
-
-
-
-def understand_aspect_ratio(injection_object, T_new, angle, aspect_ratio, bbox, path, name):
-    """visualization for debug"""
-        
-    _, verts = injection_object.get_extreme_pixels(angle=angle, image_shape=(3, 1024, 1024))
-
-    R = injection_object.get_R(-angle)
-    R = R.to(device)
-
-    V_R_1, V_R_2, V_R_3 = injection_object._get_rotated_verts(verts, R)
-
-    # here we do have T
-    xs = (
-        (1 / aspect_ratio) * injection_object.fov_weight
-        * (V_R_1 + T_new[0, 0])
-        / ((V_R_3 + T_new[0, 2]) * injection_object.f2)
-    )
-    ys = (
-        injection_object.fov_weight
-        * (V_R_2 + T_new[0, 1])
-        / ((V_R_3 + T_new[0, 2]) * injection_object.f2)
-    )
-
-    x_pixel = ((1 - xs) / 2.0) * 1024 
-    y_pixel = ((1 - ys) / 2.0) * 1024
-
-    x_pixel_int = np.clip(x_pixel.cpu().numpy(), 0, 1024 - 1)
-    y_pixel_int = np.clip(y_pixel.cpu().numpy(), 0, 1024 - 1)
-
-    pix_x = x_pixel_int
-    pix_y = y_pixel_int
-
-    image = injection_object.render_mesh(
-        angle=angle, T_z=3, T=T_new, R=R, aspect_ratio=aspect_ratio
-    )
-    # image = (image * 255).cpu().numpy().astype(np.uint8)
-
-    image = (image * 255).cpu().numpy().astype(np.uint8)
-    # image = np.transpose(image, (2, 0, 1))
-
-    draw_pixels(
-        image,
-        bbox[:, 0].numpy().astype(np.int32),
-        bbox[:, 1].numpy().astype(np.int32),
-        square_size=5,
-        paint_color=np.array([255, 0, 0]).astype(np.uint8),
-    )
-
-    draw_pixels(
-        image,
-        pix_y.astype(np.int32),
-        pix_x.astype(np.int32),
-        square_size=5,
-        paint_color=np.array([0, 0, 255]).astype(np.uint8),
-    )
-
-    Image.fromarray(image).save(f'{path}/{name}.png')
-    return image
-
-
-def test_injection_diffrent_aspect_ratio(obj_filename, app_path):
-
-    global IMAGE_SIZE
-
-    path = f'{app_path}/data/test_injected/trainval/images/final_bbox'
-    os.makedirs(path, exist_ok=True)
-    debug_path = f'{app_path}/data/test_injected/trainval/images/final_aspect'
-    os.makedirs(debug_path, exist_ok=True)
-
-    T_0 = [0, 0, 0, 0]
-
-    for angle in (0, -20, -45, -60):
-
-
-        for i, bbox in enumerate(
-            [torch.tensor([[444, 359], [444, 578], [393, 359], [393, 578]]),
-             torch.tensor([[494, 359], [494, 578], [393, 359], [393, 578]]),
-             torch.tensor([[494 + 40, 359 + 20], [494 + 40, 578 + 20], [393 + 40, 359 + 20], [393 + 40, 578 + 20]]),
-             torch.tensor([[494 + 200, 359 - 150], [494 + 200, 578 - 150], [393 + 200, 359 - 150], [393 + 200, 578 - 150]])]
-        ):
-            
-            
-
-            injection_object = InjectedObject(obj_filename)
-
-            bbox = injection_object.rotate_pixels(bbox.to(torch.float32), theta=torch.tensor([(np.pi * angle) / 180]))
-            print(f"real angle - {angle}")
-            injection_object(top_left=bbox[2], top_right=bbox[3], bottom_left=bbox[0], bottom_right=bbox[1], image_shape=[3, 1024, 1024], path=path, i=i)
-            
-            # (R_new, T_new, extreme_pixels, aspect_ratio) = (
-            #     injection_object.find_R_T_for_injection(
-            #         bottom_left=bbox[0],
-            #         bottom_right=bbox[1],
-            #         top_left=bbox[2],
-            #         top_right=bbox[3],
-            #         image_shape=[3, IMAGE_SIZE, IMAGE_SIZE],
-            #         return_aspect_ratio=True,
-            #     )
-            # )
-            # if angle == 0 and (i == 0 or i == 1 or i == 2 or i == 3):
-            #     T_0[i] = T_new
-
-            # print(f"T_new is {T_new}")
-            # print(F"T_0 is {T_0[i]}")
-            # image = injection_object.render_mesh(
-            #     angle=angle, T_z=3, T=T_0[i], R=injection_object.base_R, aspect_ratio=aspect_ratio
-            # )
-            # # image = (image * 255).cpu().numpy().astype(np.uint8)
-            # print()
-            # import torchvision
-
-            # image = image.permute(2, 0, 1)
-            # image = torchvision.transforms.functional.rotate(image, -angle, fill=1)
-            # image = image.permute(1, 2, 0)
-            # image = (image * 255).cpu().numpy().astype(np.uint8)
-            # # image = np.transpose(image, (2, 0, 1))
-
-            # draw_pixels(
-            #     image,
-            #     bbox[:, 0].numpy().astype(np.int32),
-            #     bbox[:, 1].numpy().astype(np.int32),
-            #     square_size=5,
-            #     paint_color=np.array([255, 0, 0]).astype(np.uint8),
-            # )
-
-
-            # Image.fromarray(image).save(f'{path}/bbox_{i}_angle{angle}_t0.png')
-
-            # understand_aspect_ratio(injection_object, T_new, angle, aspect_ratio, bbox, debug_path, f"bbox_{i}_angle{angle}_tnew")
-
-
-        print(f'\tdone angle - {angle}, bbox - {i}')
-        print()
-
-    print()
-
-
-def create_gif():
-
-    # Directory containing images
-    image_folder = f'{app_path}/data/test_injected/trainval/images/final_bbox'
-    images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
-    images.sort()  # Sort the images by name
-
-    # Load the first image to get the size
-    first_image = Image.open(os.path.join(image_folder, images[0]))
-
-    # Create a figure and axis to display the images
-    fig, ax = plt.subplots(figsize=(10, 8))
-    img_plot = ax.imshow(first_image, aspect='equal')
-
-    # Function to update the figure with a new image
-    def update(frame_id):
-        img = Image.open(os.path.join(image_folder, images[frame_id]))
-        img_plot.set_data(img)
-        return [img_plot]
-
-    # Create an animation
-    ani = animation.FuncAnimation(fig, update, frames=len(images), blit=True)
-
-    # Save the animation
-    ani.save(f'{image_folder}/movie.gif', writer='pillow', fps=2)
-
-    plt.close(fig)  # Close the figure to prevent it from displaying
-
-
-if __name__ == '__main__':
-
-    app_path = Path(__file__).parent.parent
-    dir_path = f'{app_path}/mmrotate/3Ddata/meshes'
-    os.makedirs(dir_path, exist_ok=True)
-
-    if torch.cuda.is_available():
-        device = torch.device('cuda:0')
-        torch.cuda.set_device(device)
-    else:
-        device = torch.device('cpu')
-
-    # Set paths
-    DATA_DIR = f'{app_path}/mmrotate/3Ddata/'
-    obj_filename = os.path.join(DATA_DIR, 'meshes/Container.obj')
-
-    # Load obj file
-    mesh = load_objs_as_meshes([obj_filename], device=device)
-    verts = mesh.verts_packed()  # Get the vertices of the mesh
-
-    test_injection_sanity_check(obj_filename, app_path)
-    # test_injection_diffrent_aspect_ratio(obj_filename, app_path)
-
+        return image, segmantation_mask
